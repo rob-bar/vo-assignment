4283ea1d5b7dafe34a8a81230dffcc55c7079499
Robbie Bardijn <robbie.bardijn@gmail.com>
2017-03-15 16:12:59 +0100

    master: initial commit: completed page, still some flaws in the components

---
+++ Readme.md
@@ -0,0 +1,23 @@
+# VO - Opdracht
+
+## Opmerkingen en werkwijze
+
+### Werkwijze
+
+* Gulp
+    Ik heb er snel wat taken ingestoken om snel te kunnen builden en om snel een page te kunnen opzetten met wat extra tools. En eventueel extra preprocessors.
+
+* Usage of html css and js
+
+    Voorlopig in de opdracht gebruik ik nog niets dat echt de componeneten appart gaat zetten,
+    volgens mij is dit ook niet het hoofddoel van de opdracht, ik zou alles in **vuejs** of **angular2** components kunnen steken maar omdat alles al is voorgekauwd door jullie lijkt mij dat overkill. 
+
+### Opmerkingen & improvements on the framework
+
+* Real webcomponents instead of full inclusion and html snippets. Using the shadow dom more.
+* BEM subtree issues propably by using the concat opperator in scss.
+* Unclear grid
+* Too less base styling, `<h2 class="h2">`
+* inlcude more icons. (font-awesome or fontello).
+* Dynamic and included breakpoints trough json config for global layout and theme layer.
+* lack of color helpers...example: -c-{colorname} example: -bgc-{colorname}
--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -24,6 +24,13 @@ import notifier from "terminal-notifier";
 import browserSync from "browser-sync";
 import gulpSequence from "gulp-sequence";
 
+// SASS
+import sass from "gulp-sass";
+import sassGlob from "gulp-sass-glob";
+import prefix from "gulp-autoprefixer";
+import sourcemaps from "gulp-sourcemaps";
+import scsslint from 'gulp-scss-lint';
+
 // LOAD CONFIGURATION FILES
 var config = require("./gulp.config.json");
 
@@ -54,6 +61,34 @@ gulp.task("scss-lint", () => {
     .pipe(scsslint());
 });
 
+// -----------------------------------------------------------------------------
+// SASS -- https://www.npmjs.com/package/gulp-sass
+// SASS GLOBBING -- https://www.npmjs.com/package/gulp-sass-glob
+// -----------------------------------------------------------------------------
+
+/**
+ * Compiles your SCSS files to CSS
+ *
+ * @task {sass}
+ * @order {2}
+ */
+
+gulp.task("sass",  () => {
+  gulp.src(config.path.src + "/scss" + "/*.scss")
+    .pipe(sourcemaps.init())
+    .pipe(sassGlob())
+    .pipe(sass({
+      outputStyle: config.sass.style
+    }))
+    .on("error", function (err) {
+      console.log(err);
+      return this.emit("end");
+    })
+    .pipe(prefix(config.autoprefixer))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(config.path.dist + "/css"))
+    .pipe(browserSync.stream());
+});
 
 // -----------------------------------------------------------------------------
 // BROWSERSYNC SERVE -- http://www.browsersync.io/docs/gulp/
@@ -67,19 +102,26 @@ gulp.task("scss-lint", () => {
  */
 gulp.task("serve",() => {
   browserSync.init({
-    server: {
-      baseDir: config.path.src,
-      startPath: "/index.html"
-    }
+    server: "./",
+    startPath: "dist/index.html"
   });
 });
 
+// -----------------------------------------------------------------------------
+// WATCH
+// -----------------------------------------------------------------------------
+
+gulp.task("watch", () => {
+  gulp.watch(config.path.src + "/scss" + "/**/*.scss", ["sass"]);
+});
 
 // -----------------------------------------------------------------------------
 // DEFAULT GULP TASK - https://www.npmjs.com/package/gulp-sequence
 // -----------------------------------------------------------------------------
 gulp.task("default", gulpSequence(
     "help",
-    "serve"
+    "sass",
+    "serve",
+    "watch"
   )
 );
